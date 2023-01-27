ed76ca2fce7fc193e046427fe50f4ac54fd1faba
Sharon Rotgaizer <62396923+sharon846@users.noreply.github.com>
2023-01-27 22:12:58 +0200

    Upload version 1

---
+++ popup.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', MainActionButtonClick, false);
+}, false);
+
+var url = "";
+
+function MainActionButtonClick(){
+	
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		url = tabs[0].url;
+		chrome.scripting.executeScript( { target: { tabId: tabs[0].id }, func: () => document.documentElement.outerHTML }, function(html) {
+			parse_data(html[0].result);
+		});
+	});
+	
+}
+
+function parse_data(data)
+{
+	if (data == "") { alert("failed"); return false; }
+	
+	var doc = new DOMParser().parseFromString(data, "text/html")
+	var divs = doc.querySelectorAll('div.info-box-content');
+	
+	if (divs.length == 0)
+		document.getElementById('err').innerText = "No videos found..";
+	else
+	{
+		document.getElementById('err').innerText = "";
+		document.getElementById('checkPage').hidden = "hidden";
+	}
+
+	for (var i = 0; i < divs.length; i+=2)
+	{
+		let btn = document.createElement("button");
+		btn.className = "video";
+		btn.innerHTML = divs[i].querySelector("p").innerText.replace('T','\n').replace('Z',"");
+		btn.title = divs[i].innerText;
+		var id = divs[i].previousElementSibling.href.split("file=")[1];
+		btn.onclick = function() {
+			window.open(url + '/d.php?file=' + id, '_blank');
+		};
+		document.body.appendChild(btn);
+	}
+}
+
